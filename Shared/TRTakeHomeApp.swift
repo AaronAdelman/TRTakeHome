2023ca0679540c7b5ebefc4aa66ff13250fe8548
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-07-05 09:36:14 +0300

    Initial Commit

---
+++ Shared/TRTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRTakeHomeApp.swift
+//  Shared
+//
+//  Created by אהרן שלמה אדלמן on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TRTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
