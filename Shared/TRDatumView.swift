0a51b381f46b01ccce04b6b2ffed7b5f1501050b
Aaron Solomon Adelman <Aaron.Solomon.Adelman@gmail.com>
2021-07-05 16:09:20 +0300

    Added Web views for articles.

--- Shared/TRDatumView.swift
+++ Shared/TRDatumView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 import Kingfisher
+import WebKit
+import Foundation
 
 struct TRDatumView: View {
     var datum: Datum
@@ -14,49 +16,52 @@ struct TRDatumView: View {
     var body: some View {
         let nameAndDateString = datum.author.name + " / " + datum.date.formattedFromTimestamp
         
-        VStack(alignment: .leading) {
-            let imageSource = datum.image.src
-            if imageSource != nil {
-                let imageSourceURL: URL? = URL(string: imageSource!)
-                if imageSourceURL != nil {
-                    KFImage(imageSourceURL!)
-                        .placeholder({
-                            TRProgressView()
-                        })
-                }
-            }
-            
-            
-            HStack {
-                let authorImageSource = datum.author.image.src
-                if authorImageSource != nil {
-                    let authorImageSourceURL: URL? = URL(string: authorImageSource!)
-                    if authorImageSourceURL != nil {
-                        KFImage(authorImageSourceURL!)
+        let request = URLRequest(url: URL(string: datum.link)!)
+        NavigationLink(destination: WebView(request: request)) {
+            VStack(alignment: .leading) {
+                let imageSource = datum.image.src
+                if imageSource != nil {
+                    let imageSourceURL: URL? = URL(string: imageSource!)
+                    if imageSourceURL != nil {
+                        KFImage(imageSourceURL!)
                             .placeholder({
                                 TRProgressView()
                             })
-                            .clipShape(Circle())
-                            .overlay(Circle()
-                                        .stroke(Color.black, lineWidth: 2.0))
                     }
                 }
                 
-                VStack(alignment: .leading) {
-                    Text(datum.title)
-                        .font(.title)
-                        .lineLimit(1)
+                
+                HStack {
+                    let authorImageSource = datum.author.image.src
+                    if authorImageSource != nil {
+                        let authorImageSourceURL: URL? = URL(string: authorImageSource!)
+                        if authorImageSourceURL != nil {
+                            KFImage(authorImageSourceURL!)
+                                .placeholder({
+                                    TRProgressView()
+                                })
+                                .clipShape(Circle())
+                                .overlay(Circle()
+                                            .stroke(Color.black, lineWidth: 2.0))
+                        }
+                    }
                     
-                    Text(nameAndDateString)
-                        .lineLimit(1)
-                } // VStack
-            } // HStack
-            
-            Text(datum.datumDescription.withoutHTMLTags())
-                .lineLimit(2)
-                .multilineTextAlignment(.leading)
-                .truncationMode(.tail)
-        } // VStack
+                    VStack(alignment: .leading) {
+                        Text(datum.title)
+                            .font(.title)
+                            .lineLimit(1)
+                        
+                        Text(nameAndDateString)
+                            .lineLimit(1)
+                    } // VStack
+                } // HStack
+                
+                Text(datum.datumDescription.withoutHTMLTags())
+                    .lineLimit(2)
+                    .multilineTextAlignment(.leading)
+                    .truncationMode(.tail)
+            } // VStack
+        }
     }
 }
 
